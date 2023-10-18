79a8c34c2fa6eb6ad6068867e5c83a0954e4bb3a
crowdbotics-apps <team@crowdbotics.com>
2023-10-18 19:25:00 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import EMPRESA
+admin.site.register(EMPRESA)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class EMPRESA(models.Model):
+    'Generated Model'
+    id_empresa = models.IntegerField(null=True,blank=True,)
+    direccion = models.TextField(null=True,blank=True,)
+    nombre = models.CharField(max_length=180,null=True,blank=True,)
